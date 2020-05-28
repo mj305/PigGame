d13f65f116cf8e046f7789be4b99b1767647e831
mj305 <maria.j.beckles@gmail.com>
2020-05-28 17:29:18 -0400

    added toggle to switch between players and restart score when dice === 1

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ let scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -38,7 +38,30 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   diceDOM.src = 'dice-' + dice + '.png';
 
   //Update the round score IF the rolled number was NOT  a 1
-})
+  if(dice !== 1){
+    //Add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  }else {
+    //Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;    
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    
+
+    /* document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.add('active'); */
+
+    document.querySelector('.dice').style.display = 'none';
+    
+  }
+
+});
 
 
 
@@ -50,7 +73,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 
 
-//document.querySelector('#current-' + activePlayer).textContent = dice;
+
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 //const x = document.querySelector('#score-0').textContent;
 
