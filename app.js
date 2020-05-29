3a3544321a12e69c30583e48c75bd3a4da242630
mj305 <maria.j.beckles@gmail.com>
2020-05-28 20:19:17 -0400

    game stops rolling dice after winner called

--- app.js
+++ app.js
@@ -1,57 +1,72 @@
 /*
 GAME RULES:
-
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-
 */
 
-let scores, roundScore, activePlayer;
+let scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', () => {
-  
+   
+  if(gamePlaying) {
+
+    //1. Random number
   let dice = Math.floor(Math.random() * 6) + 1;
 
+  //2. Display result
   let diceDOM = document.querySelector('.dice');
   diceDOM.style.display = 'block';
   diceDOM.src = 'dice-' + dice + '.png';
 
+  //3. Update the round score IF the rolled number was NOT a 1
   if(dice !== 1){
-  
+    //Add score
     roundScore += dice;
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
+    //Next player  
     nextPlayer();
    }
+  } 
 });
 
 
 document.querySelector('.btn-hold').addEventListener('click', () => {
     
+    if (gamePlaying) {
+
+      //Add CURRENT score to GLOBAL score
     scores[activePlayer] += roundScore;
     
+    //Upsate the UI
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+    //Check if player won teh game
     if(scores[activePlayer] >= 20) {
       document.querySelector('#name-' + activePlayer).textContent = 'You Win!';
       document.querySelector('.dice').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
       
+      gamePlaying = false;
+      
     } else {
+      //Next player
       nextPlayer();  
     }
+  }
 });
 
 
 
 const nextPlayer = () => {
 
+    //Next Player
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;    
     roundScore = 0;
 
