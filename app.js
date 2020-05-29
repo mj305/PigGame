8937d30b94cc17602d73a700b663e5e0949fecca
mj305 <maria.j.beckles@gmail.com>
2020-05-28 20:10:45 -0400

    reset score

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
-/* GAME RULES:
+/*
+GAME RULES:
 
-- The game has 2 players, playing in rounds.
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
 
-- In each turn, a player rolls a dice as many times as he whishes. 
-Each result get added to his ROUND score.
-
-- BUT, if the player rolls a 1, all his ROUND score gets lost. 
-After that, it's the next player's turn.
-
-- The player can choose to 'Hold', which means that his ROUND score 
-gets added to his GLBAL score. After that, it's the next player's turn.
-
-- The first player to reach 100 points on GLOBAL score wins the game. */
+*/
 
 let scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', () => {
   
@@ -38,14 +24,11 @@ document.querySelector('.btn-roll').addEventListener('click', () => {
   if(dice !== 1){
   
     roundScore += dice;
-
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-    
     nextPlayer();
    }
 });
-
 
 
 document.querySelector('.btn-hold').addEventListener('click', () => {
@@ -59,7 +42,7 @@ document.querySelector('.btn-hold').addEventListener('click', () => {
       document.querySelector('.dice').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-
+      
     } else {
       nextPlayer();  
     }
@@ -79,6 +62,28 @@ const nextPlayer = () => {
     document.querySelector('.player-1-panel').classList.toggle('active');
 
     document.querySelector('.dice').style.display = 'none';
- };  
+ }
 
+    document.querySelector('.btn-new').addEventListener('click', init);
+
+    function init() {
+      scores = [0, 0];
+      activePlayer = 0;
+      roundScore = 0;
+      gamePlaying = true;
+      
+      document.querySelector('.dice').style.display = 'none';
+  
+      document.getElementById('score-0').textContent = '0';
+      document.getElementById('score-1').textContent = '0';
+      document.getElementById('current-0').textContent = '0';
+      document.getElementById('current-1').textContent = '0';
+      document.getElementById('name-0').textContent = 'Player 1';
+      document.getElementById('name-1').textContent = 'Player 2';
+      document.querySelector('.player-0-panel').classList.remove('winner');
+      document.querySelector('.player-1-panel').classList.remove('winner');
+      document.querySelector('.player-0-panel').classList.remove('active');
+      document.querySelector('.player-1-panel').classList.remove('active');
+      document.querySelector('.player-0-panel').classList.add('active');
+  }
 
